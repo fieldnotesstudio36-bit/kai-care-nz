2059019922d81060f15362ae0f117b4ce9b49a71
fieldnotesstudio36-bit <fieldnotes.studio36@gmail.com>
2025-12-20 12:02:00 +0100

    Add files via upload

---
+++ Community-services-nz/service-worker.js
@@ -0,0 +1,21 @@
+// Service Worker for Kai & Care NZ
+const CACHE_NAME = 'kai-care-nz-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/kai-care-nz/',
+        '/kai-care-nz/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
